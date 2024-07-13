b8403be1168e61c4887d92c57402ee0f35f74678
KilluaAoki <zhy365641@gmail.com>
2024-07-13 19:37:37 +0800

    Update most configurations

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -6,7 +6,17 @@ CFG_FILE="./package/base-files/files/bin/config_generate"
 sed -i "s/luci-theme-bootstrap/luci-theme-$WRT_THEME/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
 
 # 修改默认 IP 地址
-sed -i "s/192\.168\.1\.1/$WRT_IP/g" $CFG_FILE
+sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $CFG_FILE
 
 # 修改默认主机名
 sed -i "s/hostname='.*'/hostname='$WRT_NAME'/g" $CFG_FILE
+
+# 修改 immortalwrt.lan 关联 IP
+sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
+
+# 添加编译日期标识
+sed -i "s/(\(luciversion || ''\))/(\1) + (' \/ $WRT_REPO-$WRT_DATE')/g" $(find ./feeds/luci/modules/luci-mod-status/ -type f -name "10_system.js")
+
+# 配置文件修改
+echo "CONFIG_PACKAGE_luci-theme-$WRT_THEME=y" >> ./.config
+echo "CONFIG_PACKAGE_luci-app-$WRT_THEME-config=y" >> ./.config
